26dc43debaaa8bc965da31134b5571e76089e646
JiHo Jeon <wjswlgh96@naver.com>
2025-02-14 12:29:00 +0900

    first commit

---
+++ src/main/groovy/com/newsfeed/NewsFeedApplication.groovy
@@ -0,0 +1,13 @@
+package com.newsfeed
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class NewsFeedApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(NewsFeedApplication, args)
+    }
+
+}
