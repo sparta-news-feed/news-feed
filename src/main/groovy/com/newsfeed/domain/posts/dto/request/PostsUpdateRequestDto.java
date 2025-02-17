123b70cae53ff1890cd4d1e1620103e9a761a211
wjswlgh96 <59477979+wjswlgh96@users.noreply.github.com>
2025-02-17 13:47:05 +0900

    Revert "PR 오류 병합중"

--- src/main/groovy/com/newsfeed/domain/posts/dto/request/PostsUpdateRequestDto.java
+++
@@ -1,9 +0,0 @@
-package com.newsfeed.domain.posts.dto.request;
-
-import lombok.Getter;
-
-@Getter
-public class PostsUpdateRequestDto {
-    private String title;
-    private String contents;
-}
--- src/main/groovy/com/newsfeed/domain/posts/repository/PostsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.newsfeed.domain.posts.repository;
-
-import com.newsfeed.domain.posts.entity.Posts;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostsRepository extends JpaRepository<Posts, Long> {
-}
