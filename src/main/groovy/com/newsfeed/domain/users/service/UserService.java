4238b3715b3c8369f5020f6671928fecd3c36783
JiHo Jeon <wjswlgh96@naver.com>
2025-02-18 16:48:26 +0900

    fix: UserService 메서드 public으로 수정

--- src/main/groovy/com/newsfeed/domain/users/service/UserService.java
+++ src/main/groovy/com/newsfeed/domain/users/service/UserService.java
@@ -9,12 +9,10 @@ import com.newsfeed.domain.users.dto.response.UserFollowingsProfileResponseDto;
 import com.newsfeed.domain.users.dto.response.UserProfileResponseDto;
 import com.newsfeed.domain.users.entity.User;
 import com.newsfeed.domain.users.repository.UserRepository;
-import jakarta.validation.constraints.Email;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -39,7 +37,7 @@ public class UserService {
 
   // 유저 로그인
   public UserProfileResponseDto login(String email, String password) {
-    User findUser = findByEmailOrThrow(email);
+    User findUser = findByEmailOrElseThrow(email);
     if (!passwordEncoder.matches(password, findUser.getPassword())) {
       throw new ApplicationException("비밀번호가 일치하지 않습니다", HttpStatus.UNAUTHORIZED);
     }
@@ -49,7 +47,7 @@ public class UserService {
   // 유저 단건 조회
   public UserProfileResponseDto findById(Long userId, Long targetUserId) {
 
-    User findUser = findUserById(targetUserId);
+    User findUser = findUserByIdOrElseThrow(targetUserId);
     // 팔로워와 팔로잉수 가져오기
     Long followerCount = followerRepository.countByFollowing(findUser);
     Long followingCount = followerRepository.countByFollower(findUser);
@@ -82,7 +80,7 @@ public class UserService {
 
   // 유저 비밀번호 수정
   public void updatePassword(Long userId, String oldPassword, String newPassword) {
-    User findUser = findUserById(userId);
+    User findUser = findUserByIdOrElseThrow(userId);
     // 비밀번호 수정 시, 본인 확인을 위해 입력한 현재 비밀번호가 일치하지 않은 경우 예외처리
     if (!passwordEncoder.matches(oldPassword, findUser.getPassword())) {
       throw new ApplicationException("기존의 비밀번호가 일치하지 않습니다.", HttpStatus.UNAUTHORIZED);
@@ -108,7 +106,7 @@ public class UserService {
 
   @Transactional
   public void deleteUser(Long userId, String password) {
-    User findUser = findUserById(userId);
+    User findUser = findUserByIdOrElseThrow(userId);
     if (findUser.getDeletedAt() != null) {
       throw new ApplicationException("이미 탈퇴한 사용자입니다.", HttpStatus.BAD_REQUEST);
     }
@@ -122,7 +120,7 @@ public class UserService {
 
   // 유저의 팔로잉 목록 조회 리스트
   public List<UserFollowingsProfileResponseDto> getFollowingList(Long userId) {
-    User loginUser = findUserById(userId);
+    User loginUser = findUserByIdOrElseThrow(userId);
     List<Follower> followingList = followerRepository.findByFollower(loginUser); // 내가 팔로우한 사람들
     List<UserFollowingsProfileResponseDto> responseDtos = new ArrayList<>();
 
@@ -134,7 +132,7 @@ public class UserService {
 
   // 유저의 팔로워 목록 조회
   public List<UserFollowingsProfileResponseDto> getFollowerList(Long userId) {
-    User loginUser = findUserById(userId);
+    User loginUser = findUserByIdOrElseThrow(userId);
     List<Follower> followerList = followerRepository.findByFollowing(loginUser); // 나를 팔로우한 사람들
     List<UserFollowingsProfileResponseDto> responseDtos = new ArrayList<>();
 
@@ -150,13 +148,13 @@ public class UserService {
   }
 
   // Service 레벨에서 NULL 체크 (유저 ID)
-  private User findUserById(Long userId) {
+  public User findUserByIdOrElseThrow(Long userId) {
     return userRepository.findById(userId)
         .orElseThrow(() -> new ApplicationException("해당 아이디와 일치하는 유저가 없습니다. id = " + userId, HttpStatus.NOT_FOUND));
   }
 
   // Service 레벨에서 NULL 체크 (유저 이메일)
-  private User findByEmailOrThrow(String email) {
+  public User findByEmailOrElseThrow(String email) {
     return userRepository.findByEmail(email)
         .orElseThrow(() -> new ApplicationException("해당 이메일과 일치하는 유저가 없습니다. email = " + email, HttpStatus.UNAUTHORIZED));
   }
